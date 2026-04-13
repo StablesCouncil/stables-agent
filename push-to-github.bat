8e65608ecb1257d6951f7a36c900b63ec2779fc5
Stables Council <StablesCouncil@protonmail.com>
2026-04-13 21:08:56 +0300

    Automated Backup Sync: 2026-04-13_2106

--- push-to-github.bat
+++ push-to-github.bat
@@ -1,4 +1,4 @@
 @echo off
 setlocal
 cd /d "%~dp0"
-powershell.exe -NoProfile -ExecutionPolicy Bypass -File "1_development\stream_3_governance\task_dev_utils\tools\sync-stables.ps1" -Message "One-click manual sync"
+powershell.exe -NoProfile -ExecutionPolicy Bypass -File "1_development\stream_3_governance\task_dev_utils\tools\sync-stables.ps1" -Message "One-click manual sync" -AlsoPushWhenClean
